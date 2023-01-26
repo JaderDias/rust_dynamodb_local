cd1c173eff25a1879f511f1849e8a130b97c2f49
Jader Dias <52470+JaderDias@users.noreply.github.com>
2023-01-26 18:00:41 +0100

    simplify dist path

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 mkdir -p dist/x86_64-unknown-linux-musl
 if uname -a | grep x86_64; then
     cargo build --release && \
-    cp target/release/rust_lambda dist/x86_64-unknown-linux-musl/bootstrap
+    cp target/release/rust_lambda dist/x86_64/bootstrap
 else
     rustup target install x86_64-unknown-linux-musl && \
     TARGET_CC=x86_64-linux-musl-gcc RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" cargo build --release --target x86_64-unknown-linux-musl && \
-    cp target/x86_64-unknown-linux-musl/release/rust_lambda dist/x86_64-unknown-linux-musl/bootstrap
+    cp target/x86_64-unknown-linux-musl/release/rust_lambda dist/x86_64/bootstrap
 fi
 docker-compose kill && \
 docker-compose up --build -d && \
