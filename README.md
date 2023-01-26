67f9356dac30477d434a37c080995a627a310dd8
Jader Dias <52470+JaderDias@users.noreply.github.com>
2023-01-26 10:59:35 +0100

    Add x86_64 host support

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # rust_dynamodb_local
 An example of how to test the integration of Rust with DynamoDb on your local development machine or in a Continuous Integration environment
 
-## macOS
-
-### Requirements
+## Requirements
 
 * Docker Desktop up and running
 * Rust toolchain
+
+## macOS with Apple Silicon requirements
+
 * musl-cross with x86_64
-```
+```bash
 brew install filosottile/musl-cross/musl-cross --with-x86_64
 ```
 
-### Run tests locally
+## Run tests locally
 
-```zsh
+```bash
 ./run.sh
 ```
--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
-#!/bin/zsh
-rustup target install x86_64-unknown-linux-musl && \
-TARGET_CC=x86_64-linux-musl-gcc RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" cargo build --release --target x86_64-unknown-linux-musl && \
-mkdir -p dist/x86_64-unknown-linux-musl && \
-cp target/x86_64-unknown-linux-musl/release/rust_lambda dist/x86_64-unknown-linux-musl/bootstrap && \
+#!/bin/bash
+mkdir -p dist/x86_64-unknown-linux-musl
+if uname -a | grep x86_64; then
+    cargo build --release && \
+    cp target/release/rust_lambda dist/x86_64-unknown-linux-musl/bootstrap
+else
+    rustup target install x86_64-unknown-linux-musl && \
+    TARGET_CC=x86_64-linux-musl-gcc RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" cargo build --release --target x86_64-unknown-linux-musl && \
+    cp target/x86_64-unknown-linux-musl/release/rust_lambda dist/x86_64-unknown-linux-musl/bootstrap
+fi
 docker-compose kill && \
 docker-compose up --build -d && \
 cargo run --example test
