a80fd9eb26c481e2056b4b28cbf14e4d4a5fbe7c
Jader Dias <52470+JaderDias@users.noreply.github.com>
2023-02-03 18:40:03 +0100

    run cargo audit on CI

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,3 @@
-cargo run --example test http://localhost:8080
+cargo run --example test http://localhost:8080 \
+&& cargo fmt --all -- --check \
+&& cargo clippy --all -- -W clippy::all -W clippy::pedantic
