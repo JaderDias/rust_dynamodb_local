9ccf87784e844bfde4c2077778eeb2d3b7cc894b
JaderDias <52470+JaderDias@users.noreply.github.com>
2023-02-08 23:06:37 +0100

    clippy: add more static code analysis

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,10 @@
 cargo run --example test http://localhost:8080 \
 && cargo fmt --all -- --check \
-&& cargo clippy --all -- -W clippy::all -W clippy::pedantic
+&& cargo clippy --all -- \
+  -D clippy::all \
+  -D clippy::pedantic \
+  -D clippy::cargo \
+  -D clippy::nursery \
+  -W clippy::no_effect_underscore_binding \
+  -W clippy::multiple_crate_versions \
+  -W clippy::future_not_send
